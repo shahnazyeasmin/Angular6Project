3ee40c5baa41cf95202a30ee4563fa057ddb14a1
Shahnaz Yeasmin <shahnaz.emi22@gmail.com>
2019-10-09 16:48:54 +0600

    Tutorial 29,30 ( create new employee)

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -16,6 +16,7 @@ export class CreateEmployeeComponent implements OnInit {
 
   employeeForm: FormGroup;
   employee: IEmployee;
+  pageTitle: string;
   validationMessages = {
     fullName: {
       'required': 'Full name is required',
@@ -72,7 +73,19 @@ export class CreateEmployeeComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
       const empId = +params.get('id');
       if (empId) {
+        this.pageTitle = 'Edit Employee';
         this.getEmployee(empId);
+      } else {
+        this.pageTitle = 'Create Employee';
+        this.employee = {
+          id: null,
+          fullName: '',
+          contactPreference: '',
+          email: '',
+          phone: null,
+          skills: []
+
+        };
       }
     });
   }
@@ -227,10 +240,17 @@ export class CreateEmployeeComponent implements OnInit {
   onSubmit(): void {
     // console.log(this.employeeForm.value);
     this.mapFormValuesToEmployeeModel();
+    if(this.employee.id){
     this.employeeService.updateEmployee(this.employee).subscribe(
       () => this.router.navigate(['list']),
       (err: any) => console.log(err)
     );
+    } else {
+      this.employeeService.addEmployee(this.employee).subscribe(
+        () => this.router.navigate(['list']),
+        (err: any) => console.log(err)
+      );
+    }
   }
   mapFormValuesToEmployeeModel() {
     this.employee.fullName = this.employeeForm.value.fullName;
